0c657e2246a4ef42f203ad2781d82f6df5abad2c
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-19 22:02:06 -0800

    add srv

---
+++ srv/srv.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <atomic>
+#include <thread>
+
+namespace srv {
+
+template <typename Dispatcher> class AutoDispatch {
+public:
+  AutoDispatch(std::unique_ptr<Dispatcher> dispatcher,
+               std::chrono::seconds timeout, std::chrono::milliseconds interval)
+      : m_dispatcher(std::move(dispatcher)) {
+    using namespace std::chrono;
+    using namespace std::this_thread;
+
+    const bool live_forever = timeout == 0s;
+    const auto end = steady_clock::now() + timeout;
+    m_thread = std::thread([=]() {
+      while (!m_signal_stop && (live_forever || steady_clock::now() < end)) {
+        m_dispatcher->dispatch();
+        sleep_for(interval);
+      }
+    });
+  }
+
+  template <typename Timeout, typename Interval>
+  AutoDispatch(std::unique_ptr<Dispatcher> dispatcher, Timeout timeout,
+               Interval interval)
+      : AutoDispatch(
+            dispatcher,
+            std::chrono::duration_cast<std::chrono::seconds>(timeout),
+            std::chrono::duration_cast<std::chrono::milliseconds>(interval)) {}
+
+  template <typename Interval>
+  AutoDispatch(std::unique_ptr<Dispatcher> dispatcher, Interval timeout)
+      : AutoDispatch(std::move(dispatcher), timeout,
+                     std::chrono::milliseconds(50)) {}
+
+  AutoDispatch(std::unique_ptr<Dispatcher> dispatcher)
+      : AutoDispatch(std::move(dispatcher), std::chrono::seconds(0)) {}
+
+  virtual ~AutoDispatch() { stop(); }
+
+  const Dispatcher &operator*() const { return *m_dispatcher; }
+
+  Dispatcher &operator*() { return *m_dispatcher; }
+
+  const Dispatcher *operator->() const { return m_dispatcher.get(); }
+
+  Dispatcher *operator->() { return m_dispatcher.get(); }
+
+  void signal_stop() { m_signal_stop = true; }
+
+  void stop() {
+    signal_stop();
+    join();
+  }
+
+  void join() {
+    if (!m_thread.joinable()) {
+      return;
+    }
+
+    m_thread.join();
+  }
+
+private:
+  std::unique_ptr<Dispatcher> m_dispatcher;
+  std::thread m_thread;
+  std::atomic_bool m_signal_stop = false;
+};
+
+}; // namespace srv
